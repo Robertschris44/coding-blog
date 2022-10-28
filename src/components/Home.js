9ad02504e28ac24b7ddf8585114cf856f34d9385
chiphvzvrd <christianipena@yahoo.com>
2022-10-27 20:47:36 -0500

    anchor tag animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,11 +4,13 @@ import Img2 from '../assets/images/travel-2.jpg';
 
 function Home() {
     return(
-        <Container id="jumbotron">
-            <Row>
-                <Image id="imgOne" src={Img2}></Image>
-            </Row>
-        </Container>
+        <section>
+            <Container id="jumbotron">
+                <Row>
+                    <Image id="imgOne" src={Img2}></Image>
+                </Row>
+            </Container>
+        </section>
     )
 }
 
