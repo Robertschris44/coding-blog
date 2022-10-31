25c9c55f260d8fc151968518a264c6ee818e7b10
Sumit Ojha <sumit.ojha.dev@gmail.com>
2022-10-31 14:45:51 -0500

    Updated code to integrate client and server

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import App from "./App";
 import ApolloClient from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import "semantic-ui-css/semantic.min.css";
 import "./App.css";
+
+import MenuBar from "./components/MenuBar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <div>
-      <h1>Hi there!</h1>
-    </div>
+    <Router>
+      <MenuBar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+    </Router>
   );
 }
 
