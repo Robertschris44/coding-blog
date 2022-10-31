7ae62cee2645a8d214ad2709b9390d2067c3d98d
Sumit Ojha <sumit.ojha.dev@gmail.com>
2022-10-31 16:17:02 -0500

    Created components

---
+++ README.md
@@ -0,0 +1,40 @@
+# Coding Blog
+
+## Description
+
+A coding blog for the tech frenzy people that includes the tutorial for the different coding fundamentals.
+
+## Table of contents
+
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [Contribution](#Contribution)
+- [Questions](#Questions)
+- [Website](#Website)
+- [Screenshots](#Screenshots)
+
+## Installation
+
+- npm install
+
+## Usage
+
+- npm start
+
+## Contribution
+
+Christian Pena
+Christopher Roberts
+Damian De la Garza
+Sumit Ojha
+
+## Questions
+
+- https://github.com/ChipHvzvrd
+- https://github.com/Damian-dlg
+- https://github.com/Robertschris44
+- https://github.com/sumitsann
+
+## Website
+
+## Screenshots
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,38 @@
 import React from "react";
+// import { useQuery } from "@apollo/react-hooks";
+// import gql from "graphql-tag";
 
 function Home() {
+  //   const { loading, data } = useQuery(FETCH_BLOGS_QUERY);
+  //   if (data) {
+  //     console.log(data);
+  //   }
   return (
     <div>
-      <h1>Register Page</h1>
+      <h1>Home Page</h1>
     </div>
   );
 }
+
+// const FETCH_BLOGS_QUERY = gql`
+//   {
+//     getBlogs {
+//       id
+//       description
+//       createdAt
+//       authorName
+//       commentCount
+//       comments {
+//         id
+//         body
+//         authorName
+//       }
+//       likeCount
+//       likes {
+//         authorName
+//       }
+//     }
+//   }
+// `;
 
 export default Home;
