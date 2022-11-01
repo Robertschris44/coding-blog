4aa543adea6934fdb46028ca08a50faf29887aea
damiandlg <damian.delagarzaf@gmail.com>
2022-11-01 18:46:30 -0500

    author route is done, know doing blog route

--- server/routes/author-routes.js
+++ server/routes/author-routes.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import Login from '../../client/src/pages/Login';
 import { getAllAuthor, signup } from '../controllers/user-controller';
 
 const router = express.Router();
 
 router.get("/". getAllAuthor);
-router.post("/login", signup);
+router.post("/signup", signup);
+router.post("/login", Login);
 export default router;
