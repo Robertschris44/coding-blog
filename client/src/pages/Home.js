b126ee3b32b288059cff83a7af0613dfc1a21af2
Sumit Ojha <sumitojha501@gmail.com>
2022-10-31 21:53:47 -0500

    New changes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,29 +1,34 @@
 import React from "react";
-// import { useQuery } from "@apollo/react-hooks";
-// import gql from "graphql-tag";
+import { useQuery } from "@apollo/client";
+import gql from "graphql-tag";
 
-import { Container, Divider, Grid, Header, Image, List, Menu, Segment, Dropdown, } from 'semantic-ui-react';
-
-
-
-
+import {
+  Container,
+  Divider,
+  Grid,
+  Header,
+  Image,
+  List,
+  Menu,
+  Segment,
+  Dropdown,
+} from "semantic-ui-react";
 
 function Home() {
-  //   const { loading, data } = useQuery(FETCH_BLOGS_QUERY);
-  //   if (data) {
-  //     console.log(data);
-  //   }
+  const { loading, data } = useQuery(FETCH_BLOGS_QUERY);
+  if (data) {
+    console.log(data);
+  }
   return (
     <div>
-
       <Menu fixed="top" inverted>
         <Container>
-          <Menu.Item as='a' header>
+          <Menu.Item as="a" header>
             Tech Blog!
           </Menu.Item>
-          <Menu.Item as='a'>Home Page</Menu.Item>
+          <Menu.Item as="a">Home Page</Menu.Item>
 
-          <Dropdown item simple text= 'Register'>
+          <Dropdown item simple text="Register">
             <Dropdown.Menu>
               <Dropdown.Item>Login</Dropdown.Item>
               <Dropdown.Item>Create Account</Dropdown.Item>
@@ -32,69 +37,75 @@ function Home() {
         </Container>
       </Menu>
 
-      <Container text style={{ marginTop: '7em' }}>
-        <Header as='h1'>HomePage</Header>
+      <Container text style={{ marginTop: "7em" }}>
+        <Header as="h1">HomePage</Header>
         <p>Welcome to TechBlog</p>
-        <p>Here people in Tech all over the world can share ideas, insights, and connect with each other</p>
+        <p>
+          Here people in Tech all over the world can share ideas, insights, and
+          connect with each other
+        </p>
       </Container>
 
-      <Segment inverted vertical style={{ margin: '5em 0em 0em', padding: '5em 0em'}}>
+      <Segment
+        inverted
+        vertical
+        style={{ margin: "5em 0em 0em", padding: "5em 0em" }}
+      >
         <Container textAlign="center">
           <Grid divided inverted stackable>
             <Grid.Column width={3}>
-              <Header inverted as='h4' content='Creators' />
+              <Header inverted as="h4" content="Creators" />
               <List link inverted>
-                <List.Item as='a'>Chris</List.Item>
-                <List.Item as='a'>Sumit</List.Item>
-                <List.Item as='a'>Damian</List.Item>
-                <List.Item as='a'>Chip</List.Item>
+                <List.Item as="a">Chris</List.Item>
+                <List.Item as="a">Sumit</List.Item>
+                <List.Item as="a">Damian</List.Item>
+                <List.Item as="a">Chip</List.Item>
               </List>
             </Grid.Column>
             <Grid.Column width={3}>
-              <Header inverted as='h4' content='GitHub Links' />
+              <Header inverted as="h4" content="GitHub Links" />
               <List link inverted>
-                <List.Item as='a'>Chris</List.Item>
-                <List.Item as='a'>Sumit</List.Item>
-                <List.Item as='a'>Damian</List.Item>
-                <List.Item as='a'>Chip</List.Item>
+                <List.Item as="a">Chris</List.Item>
+                <List.Item as="a">Sumit</List.Item>
+                <List.Item as="a">Damian</List.Item>
+                <List.Item as="a">Chip</List.Item>
               </List>
             </Grid.Column>
             <Grid.Column width={7}>
-              <Header inverted as='h4' content='Tech Blog' />
+              <Header inverted as="h4" content="Tech Blog" />
               <p>
-                Here you are safe to post your blogs and engage with other users!
+                Here you are safe to post your blogs and engage with other
+                users!
               </p>
             </Grid.Column>
           </Grid>
         </Container>
       </Segment>
-      
-      
-      <h1>Home Page</h1>
 
+      <h1>Home Page</h1>
     </div>
   );
 }
 
-// const FETCH_BLOGS_QUERY = gql`
-//   {
-//     getBlogs {
-//       id
-//       description
-//       createdAt
-//       authorName
-//       commentCount
-//       comments {
-//         id
-//         body
-//         authorName
-//       }
-//       likeCount
-//       likes {
-//         authorName
-//       }
-//     }
-//   }
-// `;
+const FETCH_BLOGS_QUERY = gql`
+  query {
+    getBlogs {
+      id
+      description
+      createdAt
+      authorName
+      likeCount
+      likes {
+        authorName
+      }
+      commentCount
+      comments {
+        id
+        body
+        authorName
+      }
+    }
+  }
+`;
 
 export default Home;
