587e15482ae57ba96fec5798e73c41f140f57b38
damiandlg <damian.delagarzaf@gmail.com>
2022-11-02 21:14:44 -0500

    author-rout ready for merge

--- routes/author-routes.js
+++ routes/author-routes.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import Login from '../schemas/resolvers/authors';
-import { getAllAuthor, signup } from '../schemas/resolvers/authors';
+import { generateToken, signup } from '../schemas/resolvers/authors';
 
 const router = express.Router();
 
-router.get("/". getAllAuthor);
+router.get("/". generateToken);
 router.post("/signup", signup);
 router.post("/login", Login);
 export default router;
--- routes/blog-routes.js
+++ routes/blog-routes.js
@@ -1,8 +1,11 @@
 import express from 'express';
-import {getBlogs} from "../src/pages/Home"
+import {getBlogs} from "../client/src/pages/Home"
+import FETCH_BLOGS_QUERY from 
 const blogRouter = express.Router();
 
 blogRouter.get("/", getBlogs);
 blogRouter.post("/", addBlog);
+blogRouter.put("/update/:id", updateBlog);
+blogRouter.get("/:id", getById);
 
 export default blogRouter;
